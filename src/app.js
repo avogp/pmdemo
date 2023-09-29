5ec6ec124a0e7674ad6315fd2c655af2d92702da
A <>
2023-09-29 15:31:26 +0800

    fixing the test case

--- src/app.js
+++ src/app.js
@@ -5,7 +5,7 @@ app.use(express.static("public"));
 
 // Test code below!
 app.get("/test", (_req, res) => {
-  res.status(200).send("Hello xworld");
+  res.status(200).send("Hello world!");
 });
 
 app.get("/", (req, res) => {
